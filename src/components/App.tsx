8af22b6230b103f3ee3e8d9cc717326158ceb74d
olfspnnr <olaf@uspinner.de>
2018-09-18 22:52:19 +0200

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface AppProps {}
+
+export interface AppState {}
+
+export class App extends React.Component<AppProps, AppState> {
+  render() {
+    return <div>Hallo Welt</div>;
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import "core-js";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { App } from "./components/App";
+
+ReactDOM.render(
+  <App  />,
+  document.getElementById("example")
+);
